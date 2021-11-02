97073fce61c352529230dc2318965665a9452df7
johnHan <gksdygks2124@gmail.com>
2021-11-02 10:50:26 +0900

    add PythonChallenge

---
+++ PythonChallenge/Day1.py
@@ -0,0 +1,59 @@
+"""
+As you can see, the code is broken.
+Create the missing functions, use default arguments.
+Sometimes you have to use 'return' and sometimes you dont.
+Start by creating the functions
+"""
+
+'''
+requirement:
+1)make those function:
+is_on_list
+get_x
+add_x
+remove_x
+
+2)Do not change anything inside of the "DO NOT TOUCH AREA"
+'''
+
+'''
+result:
+Is Wed on 'days' list? True
+The fourth item in 'days' is: Thu
+['Mon','Tue','Wed','Thu','Fri','Sat','Sun','Sat']
+['Tue','Wed','Thu','Fri','Sat','Sun','Sat']
+'''
+
+def is_on_list(x, y):
+    if y in x:
+        return True
+
+def get_x(x, y):
+    item = x[y]
+    return item
+
+def add_x(x, y):
+    x.append("Sat")
+    return 0
+
+def remove_x(x, y):
+    if y in x:
+        return x.remove(y)
+        
+
+# \/\/\/\/\/\/\  DO NOT TOUCH AREA  \/\/\/\/\/\/\ #
+
+days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+print("Is Wed on 'days' list?", is_on_list(days, "Wed"))
+
+print("The fourth item in 'days' is:", get_x(days, 3))
+
+add_x(days, "Sat")
+print(days)
+
+remove_x(days, "Mon")
+print(days)
+
+
+# /\/\/\/\/\/\/\ END DO NOT TOUCH AREA /\/\/\/\/\/\/\ #
