36cbe927be6ffba0763954e9710933cfacb5361e
johnHan <gksdygks2124@gmail.com>
2021-11-03 15:47:35 +0900

    class_property2.py modified

--- class_property2.py
+++ class_property2.py
@@ -1,15 +1,18 @@
-class Date:
-    def __init__(this, month):
-        this.innerMonth = month
-    
-    @property
-    def month(this):
-        return this.innerMonth
-    @month.setter
-    def month(this, month):
-        if 1 <= month <= 12:
-            this.innerMonth = month
+class NatureNum:
+    def __init__(self, num):
+        self.innnerNum = num
 
-today = Date(8)
-today.month = 15
-print(today.month)
+    @property
+    def num_value(self):
+        return self.innnerNum
+    
+    @num_value.setter
+    def num_value(self, num):
+        if type(num) == int and num >= 0:
+            self.innnerNum = num
+
+
+num1 = NatureNum(8)
+print(num1.num_value)
+num1.num_value = 16
+print(num1.num_value)
