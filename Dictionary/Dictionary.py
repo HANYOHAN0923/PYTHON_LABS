07f2a1097ccf50d3521c06d6629ec5f18258e688
johnHan <gksdygks2124@gmail.com>
2021-11-04 14:37:57 +0900

    cleaning Files

---
+++ Dictionary/Dictionary.py
@@ -0,0 +1,13 @@
+john = {
+    "name" : "john Han",
+    "age" : 29,
+    "nationality" : "korea",
+    "favorite food" : ["Pizza", "Hambergur"]
+}
+
+print(john["name"])
+print(john["age"])
+
+#input_dic
+john['handsome'] = True
+print(john)
